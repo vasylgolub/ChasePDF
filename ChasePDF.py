363222c7dec7fae3b2d7df1a05d8c3a71007aedb
Vasilii <55733989+vasylgolub@users.noreply.github.com>
2020-03-25 00:03:49 -0700

    Update ChasePDF.py

--- ChasePDF.py
+++ ChasePDF.py
@@ -3,12 +3,7 @@ import re
 import os
 ###################################
 
-class transaction:
-    def __init__(self, whole_string, date, store, price):
-        self.whole_string = whole_string
-        self.date = date
-        self.store = store
-        self.price = price
+
 
 def clean(unclean):
     return unclean[re.search(r'\d', str(unclean)).start():].replace(',',"")
