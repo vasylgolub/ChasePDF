953f2ecf03db3fdbae58c08bf0875cf8c1944642
vasyl golub <vasylgolub01@gmail.com>
2021-11-13 15:23:17 -0800

    Wrote a test to see if a file passed to the PDF class is actually a pdf file

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,13 +1,10 @@
 from src.pdf import Pdf
-import os
+
+file_path = "/Users/vasylgolub/Desktop/pdfs/2020/20200131-statements-7190-.pdf"
 
 
-
-
-def is_path_correct():
-    file_path = "/Users/vasylgolub/Desktop/pdfs/2020/20200131-statements-7190-.pdf"
-    assert Pdf.is_path_correct(file_path) is True
-
+def test_is_path_correct():
+    assert Pdf.is_file_a_pdf(file_path) is True
 
 # def check_if_array_of_words_is_correctly_split():
 #     pass
