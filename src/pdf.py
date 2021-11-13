d0ac75829cb9d60178f692d9f399497eca34cac4
vasyl golub <vasylgolub01@gmail.com>
2021-11-13 13:20:57 -0800

    Created a class Pdf that will extract text from a pdf file

---
+++ src/pdf.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfFileReader
+
+
+class Pdf:
+    def __init__(self, pdf_file_path):
+        # let's save the path just in case
+        self.file_path = pdf_file_path
+
+        opened_file = open(pdf_file_path, 'rb')
+        read_pdf_file = PdfFileReader(opened_file)
+        if read_pdf_file.isEncrypted:
+            read_pdf_file.decrypt("")
+
+        self.text = ''
+        # go through each page and extract text
+        for i in range(0, read_pdf_file.numPages):
+            # creating a page object
+            page = read_pdf_file.getPage(i)
+            # extracting text from page
+            self.text = self.text + " " + page.extractText()
+        opened_file.close()
+
+    def get_text(self):
+        return self.text
+
+    @staticmethod
+    def is_path_correct(file_path):
+        return True
