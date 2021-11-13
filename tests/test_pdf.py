1ff28b98ce3f5ecff843ff2b07daf6b7fef9c638
vasyl golub <vasylgolub01@gmail.com>
2021-11-13 15:24:05 -0800

    Created a test to check if the created paragraph from text is actually then equal to text. So to be sure no characters were lost.

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -6,5 +6,10 @@ file_path = "/Users/vasylgolub/Desktop/pdfs/2020/20200131-statements-7190-.pdf"
 def test_is_path_correct():
     assert Pdf.is_file_a_pdf(file_path) is True
 
-# def check_if_array_of_words_is_correctly_split():
-#     pass
+
+def test_if_the_paragraph_didnt_loose_any_chars():
+    my_pdf = Pdf(file_path)
+    original_text = my_pdf.text.strip(" ")
+    text_from_paragraph = my_pdf.get_text_in_paragraph()
+    text_from_paragraph_joined = "".join(text_from_paragraph.split("\n"))
+    assert original_text == text_from_paragraph_joined
