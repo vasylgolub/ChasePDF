5bf8f5e5b210bf382b7d5b06b252ff911fdb9f98
vasyl golub <vasylgolub01@gmail.com>
2021-11-13 18:23:25 -0800

    Created a test to check if all sections are present in the document. Some section names are: CHECKING SUMMARY, FEES, DAILY BALANCE and etc.

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -8,8 +8,16 @@ def test_is_path_correct():
 
 
 def test_if_the_paragraph_didnt_loose_any_chars():
-    my_pdf = Pdf(file_path)
     original_text = my_pdf.text.strip(" ")
     text_from_paragraph = my_pdf.get_text_in_paragraph()
     text_from_paragraph_joined = "".join(text_from_paragraph.split("\n"))
     assert original_text == text_from_paragraph_joined
+
+
+def test_if_all_sections_are_present_in_the_document():
+    result = True
+    # example: True and True and False and True: will give False value at the end to result variable
+    for each in my_pdf.document_sections:
+        is_it_present_in_text = my_pdf.text.find(each) != -1
+        result = result and is_it_present_in_text
+    assert result
