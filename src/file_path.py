ff7784a3416523ab25fa0c4d1d25bacb7f4b309d
vasyl golub <vasylgolub01@gmail.com>
2021-11-16 09:22:11 -0800

    The class sorts the list of pdf files based on month.

--- src/file_path.py
+++ src/file_path.py
@@ -13,10 +13,14 @@ class ListOfFiles:
         else:
             self.list_of_files = os.listdir(path_to_folder)
         self.remove_non_pdf_files()
-
+        self.list_of_files = self.get_sorted_list_based_on_month()
 
     def remove_non_pdf_files(self):
         self.list_of_files = [each_file
                               for each_file in self.list_of_files
                               if ".pdf" in each_file]
 
+    def get_sorted_list_based_on_month(self, list_to_sort=None):
+        if list_to_sort is None:
+            return sorted(self.list_of_files, key=lambda x: x[4:])
+        return sorted(list_to_sort, key=lambda x: x[4:])
