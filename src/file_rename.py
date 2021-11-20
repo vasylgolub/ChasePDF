5acbf34e0e0d509337b969509fbf6a882046e0a1
vasyl golub <vasylgolub01@gmail.com>
2021-11-19 17:16:32 -0800

    New Method to extract date in string format from a pdf file

--- src/file_rename.py
+++ src/file_rename.py
@@ -1,7 +1,14 @@
 import os
+from src.pdf import Pdf
 
 
 class FileRename:
     def __init__(self, file_path):
         self.file_full_path = file_path
         self.file_name = os.path.basename(self.file_full_path)
+        self.file_date = self.extract_date_from_file_pdf()
+
+    def extract_date_from_file_pdf(self):
+        the_pdf = Pdf(self.file_full_path)
+        date = the_pdf.get_date_of_this_statement()
+        return date
