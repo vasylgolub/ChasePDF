074cfe8767a3ac811a0bddd3bdaad1dbcb1c5d3c
vasyl golub <vasylgolub01@gmail.com>
2021-11-20 18:25:11 -0800

    New method: rename_file. It renames the pdf file to monthly statement date.

--- src/file_rename.py
+++ src/file_rename.py
@@ -13,7 +13,30 @@ class FileToRename:
         date = the_pdf.get_date_of_this_statement()
         return date
 
-    def rename_file(self, desired_name=None):
-        if desired_name is None:
-            desired_name = self.file_date
-        os.rename(self.file_full_path, desired_name)
+    # ------------------------------------------------------------------------------------------------
+    # Let's make this method also return a message besides renaming the file
+    def rename_file(self):
+        new_name_for_file = self.file_date
+        if self.file_was_already_renamed_to_desired_name(new_name_for_file + ".pdf", self.current_file_name):
+            return "File was already renamed to desired name"
+
+        extension = "pdf"
+        dir_path = self.get_dir_path_of_where_the_file_is_stored()
+
+        full_new_path = os.path.join(dir_path, new_name_for_file + "." + extension)
+        self.finally_rename_the_file_to_new_name(full_new_path)
+
+        return f"File renamed from {self.current_file_name} to {new_name_for_file}.{extension}"
+
+
+    @staticmethod
+    def file_was_already_renamed_to_desired_name(new, current):
+        return new == current
+
+    def get_dir_path_of_where_the_file_is_stored(self):
+        res, _ = os.path.split(self.file_full_path)
+        return res
+
+    def finally_rename_the_file_to_new_name(self, new_path):
+        os.rename(self.file_full_path, new_path)
+    # ------------------------------------------------------------------------------------------------
