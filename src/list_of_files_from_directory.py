77a4a8068cf81b85c6d62c1b8d75e8654dfdcb52
vasyl golub <vasylgolub01@gmail.com>
2021-11-21 16:53:01 -0800

    fixed bug and changed some things for now.

--- src/list_of_files_from_directory.py
+++ src/list_of_files_from_directory.py
@@ -5,34 +5,35 @@ class ListOfFilesFromDirectory:
     def __init__(self, path_to_folder):
         self.months = ['January', 'February', 'March', 'April', 'May',
                        'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.list_of_files = []
+        # self.list_of_files = []
 
-        if os.path.exists(path_to_folder):
-            self.path_to_folder = path_to_folder
-        else:
-            self.path_to_folder = "WRONG PATH"
-        if not os.path.isdir(path_to_folder):
-            self.path_to_folder = "IT'S NOT DIRECTORY"
-        else:
-            self.list_of_files = os.listdir(path_to_folder)
+        # if os.path.exists(path_to_folder):
+        #     self.path_to_folder = path_to_folder
+        # else:
+        #     self.path_to_folder = "WRONG PATH"
+        # if not os.path.isdir(path_to_folder):
+        #     self.path_to_folder = "IT'S NOT DIRECTORY"
+        # else:
+        #     self.list_of_files = os.listdir(path_to_folder)
+        self.path_to_folder = path_to_folder
+        self.files = os.listdir(path_to_folder)
         self.remove_non_pdf_files()
 
 
-    def remove_non_pdf_files(self):
-        self.list_of_files = [each_file
-                              for each_file in self.list_of_files
-                              if ".pdf" in each_file]
 
-    def get_list_of_files_with_full_path(self):
+    def remove_non_pdf_files(self):
+        self.files = [each_file for each_file in self.files if ".pdf" in each_file]
+
+    def with_full_path(self):
         result_list = []
-        for each in self.list_of_files:
+        for each in self.files:
             result_list.append(os.path.join(self.path_to_folder, each))
         return result_list
 
     # ------------------------------------------------------------------------------------------------
     def sorted(self, list_to_sort=None):
         if list_to_sort is None:
-            list_to_sort = self.list_of_files
+            list_to_sort = self.files
         hint = self.get_hint_on_how_to_sort_this_list_of_months(list_to_sort)
         ordered_list = [None] * 12
         for i in range(0, 12):
@@ -61,4 +62,3 @@ class ListOfFilesFromDirectory:
             result.append(calendar_pos)
         return result
     #------------------------------------------------------------------------------------------------
-
