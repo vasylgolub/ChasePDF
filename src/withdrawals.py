66d555b189ec4a70f96fd5b588aef794010d11fd
vasyl golub <vasylgolub01@gmail.com>
2021-12-03 12:39:09 -0800

    Moved a func to the new Class.

--- src/withdrawals.py
+++ src/withdrawals.py
@@ -1,21 +1,18 @@
 from src.withdrawals_text_cleaner import WithdrawalsTextCleaner
+from src.withdrawals_helper import WithdrawalsHelper
 
 
 class Withdrawals:
-    def __init__(self, whole_text):
-        cleaned_withdrawals_text = WithdrawalsTextCleaner(whole_text)
-        self.whole_text = whole_text
-        self.whole_text_wrapped = cleaned_withdrawals_text.get_wrapped_text()
-        self.title = self.whole_text_wrapped[:self.whole_text_wrapped.find("\n")]
-        self.list_of_transactions = cleaned_withdrawals_text.list_format_of_whole_text[1:-1]
+    def __init__(self, whole_text=None):
+        if whole_text is not None:
+            helper = WithdrawalsHelper
+            cleaned_withdrawals_text = WithdrawalsTextCleaner(whole_text)
+            self.whole_text = whole_text
+            self.whole_text_wrapped = cleaned_withdrawals_text.get_wrapped_text()
+            self.title = self.whole_text_wrapped[:self.whole_text_wrapped.find("\n")]  # The title is on top
+            self.list_of_transactions = cleaned_withdrawals_text.list[1:-1]
 
-        total_withdrawals_text = cleaned_withdrawals_text.list_format_of_whole_text[-1]
-        self.total_withdrawals = {"text": total_withdrawals_text,
-                                  "amount": self.get_total_from_this_string(total_withdrawals_text)}
+            total_withdrawals_text = cleaned_withdrawals_text.list[-1]
+            self.total_withdrawals = {"text": total_withdrawals_text,
+                                      "amount": helper.get_total_from_this_string(total_withdrawals_text)}
 
-    @staticmethod
-    def get_total_from_this_string(string):
-        dollar_sign_pos = string.find("$")
-        str_num = string[dollar_sign_pos + 1:]
-        str_num = str_num.replace(",", "")
-        return float(str_num)
