819a47c5aa259d2fcae0c8bd72c0cc924c9b85a7
vasyl golub <vasylgolub01@gmail.com>
2021-12-09 15:40:25 -0800

    the class can print the dictionary of stores and its number of times I shopped there.

--- src/tools/file_stores_list.py
+++ src/tools/file_stores_list.py
@@ -1,25 +1,30 @@
+#  The file should contain a list of all transactions.store made during a year or more.
 
 
 class FileStoresList:
     def __init__(self, full_path_to_the_file):
         self.opened_file = open(full_path_to_the_file)
-        list_transactions = self.get_list_from_file()
+        self.list_stores_i_went = self.get_list_from_file()
+        dic_stores_and_n_times = self.get_dictionary_with_n_times_i_shopped()
+        self.dic_sorted_by_times = sorted(dic_stores_and_n_times.items(), key=lambda kv: kv[1], reverse=True)
+        self.opened_file.close()
 
+    def print(self):
+        for each in self.dic_sorted_by_times:
+            print(each)
 
-        dictionary = {}
-        for transaction in list_transactions:
-            if transaction in dictionary:
-                dictionary[transaction] += 1
-            else:
-                dictionary[transaction] = 0
-
-        sorted_x = sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True)
-
-        for key in sorted_x:
-            print(key)
 
     def get_list_from_file(self):
         result = []
         for i in self.opened_file.readlines():
             result.append(i.rstrip())
+        return result
+
+    def get_dictionary_with_n_times_i_shopped(self):
+        result = {}
+        for store in self.list_stores_i_went:
+            if store in result:
+                result[store] += 1
+            else:
+                result[store] = 0
         return result
