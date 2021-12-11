6543bd160a6a435763205b606aca3e778e8fcaf3
vasyl golub <vasylgolub01@gmail.com>
2021-12-11 15:51:54 -0800

    Removed commented out code

--- src/list_of_files_from_directory.py
+++ src/list_of_files_from_directory.py
@@ -2,20 +2,11 @@ import os
 from pathlib import Path
 
 
+# To be used with a directory that has pdf files already renamed properly
 class ListOfFilesFromDirectory:
     def __init__(self, path_to_folder):
         self.months = ['January', 'February', 'March', 'April', 'May',
                        'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        # self.list_of_files = []
-
-        # if os.path.exists(path_to_folder):
-        #     self.path_to_folder = path_to_folder
-        # else:
-        #     self.path_to_folder = "WRONG PATH"
-        # if not os.path.isdir(path_to_folder):
-        #     self.path_to_folder = "IT'S NOT DIRECTORY"
-        # else:
-        #     self.list_of_files = os.listdir(path_to_folder)
         self.path_to_folder = path_to_folder
         self.files = os.listdir(path_to_folder)
         self.remove_non_pdf_files()
