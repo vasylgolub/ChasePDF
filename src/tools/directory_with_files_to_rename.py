9f385cf7388d04a12c6171f5fafab5d8f5ac751b
vasyl golub <vasylgolub01@gmail.com>
2021-12-22 14:40:25 -0800

    added a getter and a new functionality to the class that removes non pdf files from dir.

--- src/tools/directory_with_files_to_rename.py
+++ src/tools/directory_with_files_to_rename.py
@@ -9,7 +9,15 @@ class DirectoryWithFilesToRename:
         self._directory = directory_path
         self._files = os.listdir(directory_path)
 
+        # Remove files that are not pdfs
+        for each_file in self._files:
+            if each_file[-4:] != ".pdf":
+                os.remove(self._directory + "/" + each_file)
+
     def start_renaming(self):
         for each_file_name in self._files:
             f = FileToRename(self._directory + "/" + each_file_name)
             f.rename_file()
+
+    def get_list_of_target_files(self):
+        return self._files
