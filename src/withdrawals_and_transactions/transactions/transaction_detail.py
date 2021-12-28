ea7db6b46a8de76754425fdfa631b4a22e823b8c
vasyl golub <vasylgolub01@gmail.com>
2021-12-28 14:19:25 -0800

    mission complete

--- src/withdrawals_and_transactions/transactions/transaction_detail.py
+++ src/withdrawals_and_transactions/transactions/transaction_detail.py
@@ -6,13 +6,13 @@ class TransactionDetail:
     def __init__(self, whole_text=None):
         if whole_text is not None:
             cleaned_withdrawals_text = TransactionCleaner(whole_text)
-            self.cleaner = TransactionCleaner(whole_text)
-            # self.whole_text = whole_text
-            # self.whole_text_wrapped = cleaned_withdrawals_text.get_wrapped_text()
-            # self.beginning_balance = Helper.get_beginning_balance_as_dictionary(self.whole_text_wrapped)
-            # self.ending_balance = Helper.get_ending_balance_as_dictionary(self.whole_text_wrapped)
-            # self.list_of_transactions = cleaned_withdrawals_text.list[1:-1]
-            # self.total_sum_transactions = Helper.get_sum_of_all_transactions(self.list_of_transactions)
+            # self.cleaner = TransactionCleaner(whole_text)
+            self.whole_text = whole_text
+            self.whole_text_wrapped = cleaned_withdrawals_text.get_wrapped_text()
+            self.beginning_balance = Helper.get_beginning_balance_as_dictionary(self.whole_text_wrapped)
+            self.ending_balance = Helper.get_ending_balance_as_dictionary(self.whole_text_wrapped)
+            self.list_of_transactions = cleaned_withdrawals_text.list[1:-1]
+            self.total_sum_transactions = Helper.get_sum_of_all_transactions(self.list_of_transactions)
 
 
     def get_summary_in_text(self):
