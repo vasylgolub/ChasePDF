85563187d4ee149dd764f9d60b4b6927095a18e8
vasyl golub <vasylgolub01@gmail.com>
2022-01-05 09:35:53 -0800

    new file to store our views

--- src/my_page/the_base/views.py
+++ src/my_page/the_base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home pageeeee')
+
+
+def room(request):
+    return HttpResponse("Welcome to my Room")
