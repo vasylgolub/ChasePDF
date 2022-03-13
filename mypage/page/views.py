f60430f86dc508545d16db8116260c7c34a93582
vasyl golub <vasylgolub01@gmail.com>
2022-03-12 20:37:37 -0800

    new view: mainpage, that will render our page.

--- mypage/page/views.py
+++ mypage/page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def mainpage(request):
+    # return HttpResponse("ciao")
+    lista = [0, 1, 2]
+    context = {'lista': lista}
+    return render(request, 'page/mainpage.html', context)
