d3a4a07345612febcbec67823989f5e8dd7c7a16
vasyl golub <vasylgolub01@gmail.com>
2022-03-31 23:33:10 -0700

    Made changes to settings.py to be able to use static files.

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
