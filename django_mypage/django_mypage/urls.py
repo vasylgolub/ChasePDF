c126189e571a108ca3994005ffccb3e1ce94a5d5
vasyl golub <vasylgolub01@gmail.com>
2022-04-12 22:55:44 -0700

    Renamed project again.

--- django_mypage/django_mypage/urls.py
+++ django_mypage/django_mypage/urls.py
@@ -1,4 +1,4 @@
-"""django-mypage URL Configuration
+"""django_mypage URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
