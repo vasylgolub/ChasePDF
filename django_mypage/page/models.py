84b99591cdf832ff42fbaa9e5ddae9fbb55879ec
vasyl golub <vasylgolub01@gmail.com>
2022-08-21 17:26:11 -0700

    Create two models to store data. Database is also created.

--- django_mypage/page/models.py
+++ django_mypage/page/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class BankStatement(models.Model):
+    date = models.DateField('Transaction Execution Date')
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    # https://stackoverflow.com/questions/35543695/type-object-x-has-no-attribute-objects
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.description
+
+
+class ListOfStatementFiles(models.Model):
+    uploaded_statement_file = models.CharField(max_length=50)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.uploaded_statement_file
+
+
