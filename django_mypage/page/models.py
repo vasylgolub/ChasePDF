181edd02c0500a9410cf5e7802e102325c22ed80
vasyl golub <vasylgolub01@gmail.com>
2022-11-04 01:47:08 -0700

    Add foreign key to "statements" table

--- django_mypage/page/models.py
+++ django_mypage/page/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 
+class ListOfStatementFiles(models.Model):
+    uploaded_statement_file = models.CharField(max_length=50)  # It will contain just the month and year
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.uploaded_statement_file
+
+
 class BankStatement(models.Model):
     date = models.DateField('Transaction Execution Date')
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    # The statement file that it belongs
+    statement_file = models.ForeignKey(ListOfStatementFiles, on_delete=models.CASCADE, null=True)
 
     # https://stackoverflow.com/questions/35543695/type-object-x-has-no-attribute-objects
     objects = models.Manager()
@@ -13,12 +25,6 @@ class BankStatement(models.Model):
         return self.description
 
 
-class ListOfStatementFiles(models.Model):
-    uploaded_statement_file = models.CharField(max_length=50)
 
-    objects = models.Manager()
-
-    def __str__(self):
-        return self.uploaded_statement_file
 
 
