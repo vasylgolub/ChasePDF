57626b34505575150e95306006016c3540c034f2
vasyl golub <vasylgolub01@gmail.com>
2022-12-18 13:20:55 -0800

    Move Jquery function from html to a js file

--- django_mypage/static/jquery.js
+++ django_mypage/static/jquery.js
@@ -4,3 +4,28 @@ $(
   }
 
 );
+
+
+
+
+$(function(){
+     $("#kw").keyup(
+          function(){
+              typed_text = $(this).val()
+
+                $('#tr_transactions #description').each(function() {
+                  var match = $(this).html().toLowerCase().indexOf(typed_text)
+                  if (match > -1){
+                      $(this).addClass("highlightable")
+                  }else{
+                      $(this).removeClass("highlightable")
+                  }
+                  if (typed_text == ""){
+                      $(this).removeClass("highlightable")
+                  }
+
+                }
+                );
+          }
+      )}
+);
