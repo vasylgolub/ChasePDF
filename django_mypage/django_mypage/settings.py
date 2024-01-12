b5332f2a773c92b06beb308aee8de29221fc65b1
vasyl golub <vasylgolub01@gmail.com>
2024-01-12 11:12:50 -0800

    debug to false

--- django_mypage/django_mypage/settings.py
+++ django_mypage/django_mypage/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# print(BASE_DIR)
 
 # Changed sys.path so to be able to include module in src directory------------
 # EXTERNAL_LIBS_PATH = os.path.join(
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # sys.path = ["", EXTERNAL_LIBS_PATH] + \
 #     sys.path
 #------------------------------------------------------------------------------
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
