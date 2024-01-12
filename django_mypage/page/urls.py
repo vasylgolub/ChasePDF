a10da71371b5c6f90619dfa96001601c31d16045
vasyl golub <vasylgolub01@gmail.com>
2024-01-12 11:13:37 -0800

    rename: from url to urls

---
+++ django_mypage/page/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = "page"
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('result/', views.result_page, name='result'),
+    path('anotherpage/', views.another_page, name='anotherpage')
+]
+
+# <str:something>
