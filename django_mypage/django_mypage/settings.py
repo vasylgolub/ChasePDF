e3c6e393a4ab29423ad227c04c6f5c73c2c0b9e2
vasyl golub <vasylgolub01@gmail.com>
2024-04-22 09:18:13 -0700

    delete db and touch settings

--- django_mypage/django_mypage/settings.py
+++ django_mypage/django_mypage/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^z6zejmcaj(s4tz52qe(giexd$&nlyvqkk@=!2f%1nw-9d(*oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
